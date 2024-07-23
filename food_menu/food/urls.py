4426eef1e6553c848db902f6fc72b23ee0c74b53
Hossein <hosseinabedinipoco@gmail.com>
2024-07-23 19:27:18 +0330

    setup config that upload image

--- food_menu/food/urls.py
+++ food_menu/food/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = 'food'
 urlpatterns = [
     path('', views.index, name="index")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
