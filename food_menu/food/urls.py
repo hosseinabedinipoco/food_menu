1895f0e9ad1d578a663cf910e1f5715b884fc029
Hossein <hosseinabedinipoco@gmail.com>
2024-07-23 20:39:01 +0330

    implenet delete functionality

--- food_menu/food/urls.py
+++ food_menu/food/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 app_name = 'food'
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('delete/<int:id>', views.delete, name="delete")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- food_menu/food/views.py
+++ food_menu/food/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 from .models import Food
@@ -7,4 +7,17 @@ def index(request):
     foods = Food.objects.all()
     template = loader.get_template("index.html")
     context = {'foods':foods}
+    return HttpResponse(template.render(context, request))
+
+def delete(request, id):
+    food = Food.objects.get(pk=id)
+
+    if request.method == 'POST':
+        food.delete()
+        return redirect("food:index")
+    
+    template = loader.get_template("delete.html")
+    context = {
+        "food" : food
+    }
     return HttpResponse(template.render(context, request))
