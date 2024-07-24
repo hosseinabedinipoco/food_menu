5b44d3e6f87192a3c4ed97aeb60c2caef98dffa5
Hossein <hosseinabedinipoco@gmail.com>
2024-07-24 15:33:39 +0330

    add template for display the form for adding food item

--- food_menu/food/models.py
+++ food_menu/food/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Food(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     price = models.IntegerField()
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", null=False)
